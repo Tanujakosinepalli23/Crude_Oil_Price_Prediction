2ff5c5115cbfa995d87f7533277fddaa603da706
Tanujakosinepalli23 <169164986+Tanujakosinepalli23@users.noreply.github.com>
2026-03-27 14:06:42 +0530

    Update streamlit_deploy_crude_model.py

--- streamlit_deploy_crude_model.py
+++ streamlit_deploy_crude_model.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import statsmodels.tsa.arima.model import ARIMA
+from statsmodels.tsa.arima.model import ARIMA
 import matplotlib.pyplot as plt
 from pathlib import Path
 import warnings
